e741be49be038e4876dd368d5cee7bca7f702eba
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-01-02 00:43:03 +0600

    Escape Sequence

---
+++ BioData.java
@@ -0,0 +1,11 @@
+public class BioData {
+    public static void main(String[] args) {
+        // System.out.println("Joy Tarafder");
+        // System.out.println("01714890199");
+        // System.out.println("B.Sc in CSE");
+        
+        System.out.println("Joy Tarafder \n01714890199 \nB.Sc in CSE");
+        System.out.println("1 \t 2 \t 3");
+        System.out.println("\"Java Programming\"");
+    }
+}
