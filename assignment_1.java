ab905e014bb98bb2ddffbcf6251570ad38826e57
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-01-02 00:24:10 +0600

    Add files via upload

---
+++ assignment_1.java
@@ -0,0 +1,9 @@
+public class assignment_1 {
+    public static void main(String[] args) {
+        System.out.print("Id : 101");
+        System.out.print("\nTitle : iphone 15");
+        System.out.print("\nPrice : 1895 euros");
+        System.out.print("\nDescription : perfect product with best image quality");
+        System.out.print("\nCategory : Phone");
+    }
+}
