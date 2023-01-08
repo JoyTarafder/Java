503e0fb59b8d7f6c23af848fc44b9568e2d19ba7
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-01-08 19:19:04 +0600

    Name error

--- verable.java
+++
@@ -1,8 +0,0 @@
-public class verable {
-    public static void main(String[] args) {
-        int num = 156;
-        String nam = "Joy Tarafder";
-        System.out.println(num);
-        System.out.println(nam);
-    }
-}
