d19236d663cf030a6cd97d6efbce505804feadb6
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-01-08 19:19:40 +0600

    Variable

---
+++ variable.java
@@ -0,0 +1,8 @@
+public class verable {
+    public static void main(String[] args) {
+        int num = 156;
+        String nam = "Joy Tarafder";
+        System.out.println(num);
+        System.out.println(nam);
+    }
+}
