ec21d0e8cc8952743a188bb34d103aebb7ddd0e3
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-01-08 19:18:26 +0600

    Execution Steps

---
+++ ExecutionSteps.java
@@ -0,0 +1,10 @@
+public class ExecutionSteps {
+    public static void main(String[] args) {
+        System.out.println("Java\'s Phases // Execution steps.........");
+        System.out.println("1. Edit");
+        System.out.println("2. Compile");
+        System.out.println("3. Load");
+        System.out.println("4. Verify");
+        System.out.println("5. Execute");
+    }
+}
