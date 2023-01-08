c8b8ed137c08e7de29d19603f8b21b682f3da9b7
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-01-08 19:21:40 +0600

    Assignment 2

---
+++ assignment_2.java
@@ -0,0 +1,17 @@
+public class assignment2 {
+    public static void main(String[] args) {
+        int id = 101;
+        String title = "iphone15";
+        int price = 1895;
+        String description = "perfect with best image quality.";
+        String category = "phone";
+
+        System.out.println("ID = " + id);
+        System.out.println("Title = " + title);
+        System.out.println("Price = " + price);
+        System.out.println("Description = " + description);
+        System.out.println("Category = " + category);
+
+    }
+}
+
