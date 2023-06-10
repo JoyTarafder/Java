827b0eb653ac103c1f3ebc4b1282269662ed9a4f
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-06-10 11:45:55 +0600

    Print Patten

---
+++ printPatten.java
@@ -0,0 +1,9 @@
+
+public class printPatten {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
