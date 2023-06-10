04d0305f97716409230395620ecd362df6ac02f8
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-06-10 15:01:30 +0600

    String User Input

---
+++ stringInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class stringInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Input Finction
+        // String name = sc.next(); // Single word print
+        String name = sc.nextLine(); // Whole sentence print
+        System.out.println(name);
+    }
+
+}
