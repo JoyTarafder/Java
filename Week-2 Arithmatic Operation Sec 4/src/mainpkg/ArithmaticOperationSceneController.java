eafda027d3820f5109f18baca66375e490753df1
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-10-02 21:08:24 +0600

    Arithmetic Operation

---
+++ Week-2 Arithmatic Operation Sec 4/src/mainpkg/ArithmaticOperationSceneController.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Asus
+ */
+public class ArithmaticOperationSceneController implements Initializable {
+
+    @FXML    private Label resultLabel;
+    @FXML    private TextField xTextField;
+    @FXML    private TextField yTextField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void addOnMouseClick(MouseEvent event) {
+        int xVal = Integer.parseInt(xTextField.getText());
+        int yVal= Integer.parseInt(yTextField.getText());
+        int sum = xVal + yVal;
+        resultLabel.setText(
+                "X + Y : "+
+                Integer.toString(sum));
+        
+    }
+
+    @FXML
+    private void subOnMouseClick(MouseEvent event) {
+        int xVal = Integer.parseInt(xTextField.getText());
+        int yVal= Integer.parseInt(yTextField.getText());
+        int sub = xVal - yVal;
+        resultLabel.setText(
+                "X - Y : "+
+                Integer.toString(sub));
+    }
+
+    @FXML
+    private void multOnMouseClick(MouseEvent event) {
+        int xVal = Integer.parseInt(xTextField.getText());
+        int yVal= Integer.parseInt(yTextField.getText());
+        int mult = xVal * yVal;
+        resultLabel.setText(
+                "X * Y : "+
+                Integer.toString(mult));
+    }
+    
+}
